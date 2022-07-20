9417fa731430dde487eddf0168b5809f0e8b42fd
LeGrandMAG <magmukendi@hufs.ac.kr>
2022-07-21 07:39:26 +0900

    Updates

--- magportal/src/Page/Projects.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Projects() {
-  return (
-    <div>Voici mes projects</div>
-  )
-}
-
-export default Projects
--- magportal/src/components/data.js
+++
@@ -1,9 +0,0 @@
-import pp from "../assets/books/precolonialBlackAfrica.jpg"
-export default  [
-    {
-        title:"Precolonial Black Africa",
-        author:"Cheikh Anta Diop",
-        img: pp,
-        link: "https://drive.google.com/file/d/1qgEQjD8ZRZaY73DSbwOXe-8lcPQM8Gmp/view?usp=sharing"
-    }
-]
