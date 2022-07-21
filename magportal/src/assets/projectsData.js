9f7430448faea14524b5ede53aad1619b984fb00
LeGrandMAG <magmukendi@hufs.ac.kr>
2022-07-21 15:41:29 +0900

    added a new api

--- magportal/src/assets/projectsData.js
+++ magportal/src/assets/projectsData.js
@@ -7,5 +7,13 @@ export default  [
         illustration:"gi file",
         link:"link to the project",
         sourceCode: ""
+    },
+    {
+        id: nanoid(),
+        title: "Wuicky Meme",
+        description:"This is Meme generator. It allows the user to choose among famous random meme images and make a meme out of it in seconds. The user can also download the image if he wants or share it directly on social media.",
+        illustration:"./projects/meme.gif",
+        link:"https://magmukendi.me/quickymeme/",
+        sourceCode: "https://github.com/LeGrandMAG/quickymeme/tree/main/quickmeme"
     }
 ]
