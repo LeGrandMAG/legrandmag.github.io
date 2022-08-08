0c64bee51eac4e230c64d82dcd7f61e28c4bda76
LeGrandMAG <magmukendi@hufs.ac.kr>
2022-08-08 15:56:22 +0900

    edit after the actions

--- magportal/src/Page/Home.js
+++ magportal/src/Page/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div className="home-container"><br/><p>
-        Hello welcome to my portfolio website. <br/> I am currently updating the different features of this page.
+        Under development, checkout later. <br/>.
         <br/> It will be available very soon.
         </p>
         </div>
