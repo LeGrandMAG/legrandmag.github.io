dd83e8555e2cd43b5484dbebda4805f94b6297f0
LeGrandMAG <magmukendi0@gmail.com>
2022-12-05 03:40:59 +0900

    improved the project display

--- src/Page/Editor/Editor.jsx
+++ src/Page/Editor/Editor.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import './Editor.css'
 import { Editor } from '@tinymce/tinymce-react';
 import {TOPICS} from '../../data'
